ee89a32921508678a0b4fc689060796d093037fd
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-11-27 21:04:11 -0800

    dwrite a script that prints the zen of python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-import codecs
-print(codecs.decode(__import__('this').s, 'rot_13'))
+#import codecs
+#print(codecs.decode(__import__('this').s, 'rot_13'))
+import this
+#print(this.s)
