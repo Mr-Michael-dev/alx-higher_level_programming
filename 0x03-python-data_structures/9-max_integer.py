cabe2a37ededed7a999cb39484b29b88b15c26c1
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-12-04 12:45:46 +0100

    debug 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 
 def max_integer(my_list=[]):
     """Returns the biggest integer of a list."""
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return None
     else:
         new_list = sorted(my_list)
