d414a897d567caf395fe1c7f64367f7615719444
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-12-19 10:18:52 +0100

    pycodestyle debug

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,10 +3,7 @@
 def list_division(my_list_1, my_list_2, list_length):
 
     new_list = []
-    for i in range(list_length): 
-    #(element1, element2) in enumerate(zip(my_list_1, my_list_2)):
-     #   if i >= list_length:
-      #      break
+    for i in range(list_length):
         try:
             new_list.append(my_list_1[i] / my_list_2[i])
             result = 1
