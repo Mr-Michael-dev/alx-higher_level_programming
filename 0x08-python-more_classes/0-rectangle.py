856eac0c479d1cbc00d56be3a657fcb30ded0dfd
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-01-04 23:21:23 +0100

    0. Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    This module defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+
+    It currently has no attributes and no property
+    """
+    pass
