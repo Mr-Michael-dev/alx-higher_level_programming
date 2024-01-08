6d6ead4f9c1baade19b30327fbda08b0163a96bb
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-01-08 09:11:08 +0100

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module adds two integers
+a: First integer
+b: Second integer
+"""
+
+def add_integer(a, b=98):
+    """
+    Returns the sum of a and b
+    """
+    return a + b
