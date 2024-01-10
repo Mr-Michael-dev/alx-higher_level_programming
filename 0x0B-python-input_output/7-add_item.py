f5b80407657a45eb06779e6b3cf226d7e738aa57
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-01-10 07:54:56 +0100

    Load, add to and save JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-    This module adds all arguments to a Python list,
-    and then save them to a json file
-"""
+"""This module adds all arguments to a Python list,
+and then save them to a json file"""
 
 
 import sys
