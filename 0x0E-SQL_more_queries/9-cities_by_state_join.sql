da6071eeff7990010f9c30f33912e57195e6c313
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-02-16 05:48:13 +0100

    list citiea by state

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- script to list all cities contained in hbtn_0d_usa
+
+-- query to list all cities id and name by state sorted by cities.id
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
