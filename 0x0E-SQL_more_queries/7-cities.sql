a844e8e52129c512269eaef6b5393e6e2046a25a
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-02-16 05:05:21 +0100

    cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- script that creates a database and a table
+
+-- use CREATE DATABASE TO create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- switch to the newly created or existing database
+USE hbtn_0d_usa;
+
+-- use CREATE TABLE to create table with unique auogenerated id and id has primary key
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES state(id),
+	name VARCHAR(256) NOT NULL
+);
