94ca58a0f76c785b29850206547d43caeaea0cee
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-03-12 05:46:11 +0100

    object

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('node:process');
 
-function factorial(n) {
-if (isNaN(n)) {
+function factorial (n) {
+  if (isNaN(n)) {
     return 1;
   } else if (n === 0 || n === 1) {
     return 1;
@@ -12,6 +12,6 @@ if (isNaN(n)) {
   }
 }
 
-result = factorial(parseInt(argv[2]));
+const result = factorial(parseInt(argv[2]));
 
 console.log(result);
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('node:process');
 
-function add(a, b) {
-console.log(a + b)
+function add (a, b) {
+  console.log(a + b);
 }
 
 add(parseInt(argv[2]), parseInt(argv[3]));
