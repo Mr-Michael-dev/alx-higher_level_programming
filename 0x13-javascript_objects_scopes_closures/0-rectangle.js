a35d9495528d19c8730ecb5a6619055cfd76cccf
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-03-13 03:27:15 +0100

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -5,6 +5,6 @@
  */
 
 class Rectangle {
-};
+}
 
 module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/**
+ * class Rectangle that defines a rectangle
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
