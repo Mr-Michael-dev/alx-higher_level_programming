1bbae6b269888685ba529d50204a388d81d0ddf9
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-03-16 02:45:16 +0100

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE '$sys.argv[4]%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE
+                'sys.argv[4]%' ORDER BY id ASC")
 
     rows = cur.fetchall()
 
