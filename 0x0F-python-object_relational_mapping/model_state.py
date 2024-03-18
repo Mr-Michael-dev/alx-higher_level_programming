ddb4108eacb5296184d68485cb50b8a143d6b1c2
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-03-18 02:39:01 +0100

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -24,3 +24,6 @@ class State(Base):
     id = Column(Integer, primary_key=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    def __str__(self):
+        return "{}: {}".format(self.id, self.name)
