572f54d8a95603fa6224166498cc8eddc39b5ad7
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-04-24 05:35:21 +0100

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,7 +9,7 @@ const request = require('request');
 const args = process.argv;
 const episodeNumber = parseInt(args[2]);
 
-request('https://swapi-api.alx-tools.com/api/films/${episodeNumber}', (err, response, body) => {
+request(`https://swapi-api.alx-tools.com/api/films/${episodeNumber}`, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
