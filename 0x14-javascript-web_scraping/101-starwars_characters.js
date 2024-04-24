fd26ad76ae473ea47bab1023f85fbab212b6ea96
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-04-24 17:21:02 +0100

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie in the same order as the list “characters” in the /films/ response.
- */
+const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-const request = require('request-promise');
-const filmId = process.argv[2];
+request(url, async (error, response, body) => {
+  if (error) { console.log(error); }
+  const result = JSON.parse(body);
 
-(async () => {
-  try {
-    // Make request to fetch film data
-    const filmData = await request(`https://swapi-api.alx-tools.com/api/films/${filmId}/`, { json: true });
-    const characters = filmData.characters;
-
-    // Sequentially fetch character data and print names
-    for (const characterUrl of characters) {
-      const characterData = await request(characterUrl, { json: true });
-      console.log(characterData.name);
-    }
-  } catch (error) {
-    console.error('Error:', error);
+  for (const charURL of result.characters) {
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, r, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
   }
-})();
+});
