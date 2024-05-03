5c863c6d39c9dfb9f48911470cb5712a402fb4b0
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-05-03 06:07:38 +0100

    Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,16 @@
+/*
+ * fetches a URL using JQuery
+ */
+
+$(function () { 
+     $.ajax({ 
+         method: 'GET', 
+         url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json', 
+         success: function(data){ 
+             $('#character').text(data.name) 
+         }, 
+         error: function(){ 
+             console.error('Error fetching character data:', error); 
+         } 
+     }); 
+ });
