71a38eb3716646dfebf5bed794b9cfdea178e976
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-05-03 06:34:02 +0100

    advanced 101

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,17 @@
+/*
+ * adds, removes and clears LI elements from a list
+ */
+
+$(document).ready(function() {
+    $('#add_item').click(function() {
+        $('ul.my_list').append('<li>Item</li>');
+    });
+
+    $('#remove_item').click(function() {
+        $('ul.my_list li:last-child').remove();
+    });
+
+    $('#clear_list').click(function() {
+        $('ul.my_list').empty();
+    });
+});
